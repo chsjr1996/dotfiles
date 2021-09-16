2f5d41639fb4be61cbff8ee13ded9594cba3cc76
Carlos Henrique <chsjr1996@gmail.com>
2021-09-15 23:21:47 -0300

    Some fixes on polybar
    - Enable polybar IPC
    - Add script to hide polybar on startup
    - Fix modules/launcher colors variables
    - Use backlight instead of xbacklight
    - Remove 'muted' label from pulseaudio

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,3 +12,18 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
 polybar -q main -c "$DIR"/config.ini &
+
+# Hide polybar on startup
+while [ 1 ]
+do
+    sleep 1 
+    pid=`ps -ef | grep "polybar" | grep -v grep | awk ' {print $2}'`
+    if [ "$pid"!="" ]
+    then
+        polybar-msg cmd hide 
+        exit 
+    else
+        sleep 1
+    fi
+done
+
