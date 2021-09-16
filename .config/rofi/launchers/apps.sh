ba5a6766df14ba707dd0c490fd7fbf13d080b76f
Carlos Henrique <chsjr1996@gmail.com>
2021-09-15 23:23:34 -0300

    Some themes and launchers changes
    - Use sidebar style to most launchers
    - Remove unused files
    - Clean files

--- .config/rofi/launchers/apps.sh
+++ .config/rofi/launchers/apps.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
 dir="$HOME/.config/rofi/themes"
-theme="style_apps"
+theme="style_sidebar"
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
